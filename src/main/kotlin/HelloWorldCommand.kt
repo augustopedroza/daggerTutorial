eb01382794354607209dfa4c1b7a399321bfe4b2
Augusto Pedroza <augusto@route.com>
2019-11-04 14:12:17 -0700

    Removed unnecessary property key from Command

--- src/main/kotlin/HelloWorldCommand.kt
+++ src/main/kotlin/HelloWorldCommand.kt
@@ -2,7 +2,6 @@ import javax.inject.Inject
 import Command.*
 
 class HelloWorldCommand @Inject constructor(private var outputter: Outputter): Command {
-    override val key: String = "hello"
 
     override fun handleInput(input: List<String>): Status {
         if (input.isNotEmpty()) {
