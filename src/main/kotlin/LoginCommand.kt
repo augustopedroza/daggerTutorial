bdc389d5724ea113671f2467f0b6a3f97d6a5830
Augusto Pedroza <augusto.pedroza@gmail.com>
2019-11-05 08:59:07 -0700

    Only allow one active login at a time (#13)

--- src/main/kotlin/LoginCommand.kt
+++ src/main/kotlin/LoginCommand.kt
@@ -1,11 +1,24 @@
 import javax.inject.Inject
 import Command.Result
+import Database.Account
+import java.util.*
 
 class LoginCommand @Inject constructor(private val database: Database,
                                        private val outputter: Outputter,
-                                       private val userCommandsRouterFactory: UserCommandsRouter.Factory): SingleArgCommand() {
+                                       private val userCommandsRouterFactory: UserCommandsRouter.Factory,
+                                       private val account: Optional<Account>
+): SingleArgCommand() {
 
     public override fun handleArg(username: String): Result {
+        if (account.isPresent) {
+            val loggedInUser = account.get().username
+            outputter.output("$loggedInUser is already logged in")
+            if (loggedInUser != username) {
+                outputter.output("run `logout` first before trying to log in another user")
+            }
+            return Result.handled();
+        }
+
         val account = database.getAccount(username)
         outputter.output("$username is logged in with balance ${account.balance}")
         return Result.enterNestedCommandSet(
